faec738c3ef61ebb6dfeebc42daf039fe729b8f5
Carlos Elizondo <caelra99@gmail.com>
2020-02-20 01:11:50 -0600

    Add video game header

---
+++ video_game.h
@@ -0,0 +1,25 @@
+#include <string>
+
+class VideoGame {
+    public:
+      std::string name;
+      std::string esrb;
+      std::string status;
+      float price;
+      int quantity;
+
+      // Member Functions
+      void setESRB(int select);
+      void setStatus(int select);
+      void setPrice(float fixed_price);
+};
+
+void VideoGame::setESRB(int select) {
+  char classification[6][5] = { "E", "E10+", "T", "M17+", "A", "RP" };
+  esrb = classification[select];
+}
+
+void VideoGame::setStatus(int select) {
+  char statuses[2][6] = { "Venta", "Renta" };
+  status = statuses[select];
+}
