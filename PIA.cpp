b39375f1ec23bf76bad02507d7c5b18a167bd9e7
David <davidantonioelizondoramos@gmail.com>
2020-02-19 22:56:28 -0600

    Initial commit

---
+++ PIA.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int a;
+
+void main() {
+
+	cout << "introduce un numero del 1 al 2 \n";
+	cin >> a;
+
+	if (a == 1){
+		cout << "Compra de Video juego \n";
+
+    }
+	 else if (a == 2) {
+		cout << "Renta de Video juego \n";
+
+	  }
+	 else {
+		cout << "ERROR";
+	 
+	  }
+	system("pause<nul");
+}
+	      
